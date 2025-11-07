92a6b81ca94e441b2f373809d532b259f4f4164f
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-11-07 08:56:32 -0600

    Create page.tsx

---
+++ app/checkout/page.tsx
@@ -0,0 +1,10 @@
+import CheckoutForm from "@/components/checkout-form"
+
+export default function CheckoutPage() {
+  return (
+    <section className="space-y-4">
+      <h1 className="text-2xl font-bold">Checkout (DEMO)</h1>
+      <CheckoutForm />
+    </section>
+  )
+}
